1b40a2189a8671204dc031786cc466d7fbbafab5
geryfabrega <54220892+geryfabrega@users.noreply.github.com>
2022-04-18 13:42:45 -0400

    Update main.py
    serverside image processing, still working. Just need to encode the returning image into a JSON base 64 string and to send back to ajax front end

--- main.py
+++ main.py
@@ -1,39 +1,61 @@
 import re
 from flask import Flask, request, jsonify, after_this_request
+import cv2
+import numpy as np
+import base64
+from torchvision.models import detection
+import torch
 
 app = Flask(__name__)
 
 @app.route('/test', methods=['GET','POST'])
 def test():
     print(request.method)
-    if request.method == 'GET':
-        @after_this_request
-        def add_header(response):
-            response.headers.add('Access-Control-Allow-Origin', '*')
-            return response
-
-        jsonResp = {'jack': 4098, 'sape': 4139}
-        return jsonify(jsonResp)
     if request.method == 'POST':
+        request.get_data()
+        print("Data Recieved")
+        names = saveImage(request.data)
+        jsonResp = names
+        response = jsonify(jsonResp)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+        
+    if request.method == 'GET':
         print("GOAT IN THE WATER!")
-        @after_this_request
-        def add_header(response):
-            print(request.get_data())
-            parse(request.data)
-            response.headers.add('Access-Control-Allow-Origin', '*')
-            return response
-        jsonResp = {'jack': 4098, 'sape': 4139}
+        jsonResp = {'FLSGHJRLGJRSHGKRSJG': 'fglkrjghkrghrkgj'}
         return jsonify(jsonResp)
 
+def saveImage(data):
+    data = data.decode("utf-8")
+    data = data.replace("data:image/png;base64,","")
+    imgdata = base64.b64decode(data)
+    filename = 'some_image.jpg'
+    with open(filename, 'wb') as f:
+        f.write(imgdata)
+    
+    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    cascade_path = "haarcascade_frontalface_default.xml"
+    face_cascade = cv2.CascadeClassifier(cascade_path)
+    print("PROGRAM START")
+    print("Open Image in local images directory")
+    showImg(face_cascade)
 
-def parse(data):
-    data = str(data)
-    oneLine = ''.join(data)
-    res = [i for i in range(len(oneLine)) if oneLine.startswith("Profilepictureof", i)]
-    for i in range(len(res)):
-        name = oneLine[res[i]+ 19:res[i]+55]
-        name = name.split("upn")[0]
-        print(name)
+def showImg(face_cascade):
+    path = "some_image.jpg"
+    img = cv2.imread(path, 0)
+    faces = face_cascade.detectMultiScale(img)
+    color = (255,0,255)
+    color2 = (0,255,255)
+    thickness = 1
+    box_ratio = .22
+    for (x, y, w, h) in faces:
+        print("new Image!")
+        h2 = int(h * box_ratio)
+        cv2.rectangle(img, (x, y-h2), (x+w, y+h2), color2, thickness)
+        cv2.imwrite("output.jpg",img)
+
+
+
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8989)
+    app.run(host='localhost', port=8989, debug=True, threaded=True)
