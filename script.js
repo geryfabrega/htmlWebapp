59ab45e7786e5409e82ebdf3b10f21f61f2c9ce5
geryfabrega <54220892+geryfabrega@users.noreply.github.com>
2022-04-18 13:44:04 -0400

    Update script.js
    front end javascript button logic + ajax http request to send images

--- script.js
+++ script.js
@@ -1,45 +1,74 @@
-document.addEventListener("mouseover", function() {
-    document.body.style.backgroundColor = "lavender";
-});
-document.addEventListener("mouseout", function() {
-    document.body.style.backgroundColor = "white";
-});
+console.log("Hello There and Welcome to my title page");
+var id = null;
+var imageUpload = false;
+function businessCard() {
+    var elem = document.getElementById("card");
+    var pos = -10;
+    clearInterval(id);
+    id = setInterval(frame, 1);
+    function frame() {
+      if (pos == 300) {
+        clearInterval(id);
+      } else {
+        pos++;
+        elem.style.top = pos + 'px';
+      }
+    }
+  }
 
-var input = document.getElementById("customFile");
-var output = document.getElementById("output");
 
-input.addEventListener("change", function () {
-  if (this.files && this.files[0]) {
-    var myFile = this.files[0];
-    var reader = new FileReader();
-    
-    reader.addEventListener('load', function (e) {
-      output.textContent = e.target.result;
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        imageUpload = true;
+        reader.onload = function (e) {
+            $('#imageResult')
+                .attr('src', e.target.result);
+            $('#imageResult')
+                .attr('width', 400);
+            $('#imageResult')
+                .attr('height', 500);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$(function () {
+    $('#upload').on('change', function () {
+        readURL(input);
     });
-    
-    reader.readAsBinaryString(myFile);
-  }   
 });
 
-//var output = document.getElementById("output").value;
-function getHello() {
-    let myName = "Gery";
-    let myPassword = "abcde";
-    const url = 'http://localhost:8989/test'
-    fetch(url, {
-        method: "POST",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-      
-        //make sure to serialize your JSON body
-        body: JSON.stringify({
-          name: myName,
-          password: myPassword
-        })
-      })
-      .then( (response) => { 
-         //do something awesome that makes the world a better place
-      });
+/*  ==========================================
+    SHOW UPLOADED IMAGE NAME
+* ========================================== */
+var input = document.getElementById( 'upload' );
+var infoArea = document.getElementById( 'upload-label' );
+
+input.addEventListener( 'change', showFileName );
+function showFileName( event ) {
+  var input = event.srcElement;
+}
+
+function sendImage(){
+  if (imageUpload){
+    var output = document.getElementById("imageResult").src;
+    let text = output;
+    console.log(text);
+    console.log("Starting AJAX");
+    $.ajax({
+        url: "http://localhost:8989/test",
+        type: "POST",
+        data: text,
+        dataType:"json",
+        success: function(data){
+            var myData = data["Names"];
+            document.getElementById('output').value = myData;
+            console.log(myData);
+        }
+    });
+  }
+  else{
+    alert("Image must be uploaded prior to processing.")
+  }
 }
